67b5ef6e1c2626860cfc0cf01c3020f549563409
Aitor Reviriego Amor <aitorevi@gmail.com>
2023-08-19 23:20:11 +0200

    test(red): list_with_one_item

--- fashion.test.ts
+++ fashion.test.ts
@@ -1,12 +1,17 @@
-// [] -> undefined
-
 import { list } from "./fashion"
 
 describe("fashion", () => {
 
+    // [] -> undefined
     it("list_is_empty", () => {
         const listEmpty = list[0]
         expect(undefined).toBe(listEmpty)
+    })
+
+    // [1] -> 1
+    it("list_with_one_item", () => {
+        const listItem = list[0]
+        expect(1).toBe(listItem)
     })
 
 })
