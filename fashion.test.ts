16178b868f24dfd0394793ba70fedc262c81c400
Aitor Reviriego Amor <aitorevi@gmail.com>
2023-08-19 19:06:26 +0200

    test(green): list_is_empty

--- fashion.test.ts
+++ fashion.test.ts
@@ -1,8 +1,11 @@
 // [] -> undefined
 
+import { list } from "./fashion"
+
 describe("fashion", () => {
 
     it("list_is_empty", () => {
+        const listEmpty = list[0]
         expect(undefined).toBe(listEmpty)
     })
 
